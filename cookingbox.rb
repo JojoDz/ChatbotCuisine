ab74ec75eae201b723b861eac350dc5e1a376226
Marine139 <marine.luciani@student.adatechschool.fr>
2020-11-16 17:25:32 +0100

    picking chocolate cake

--- cookingbox.rb
+++ cookingbox.rb
@@ -6,4 +6,6 @@ puts "Bonjour, comment allez vous?"
 puts "Quel est votre prénom?"
 prenom = gets.chomp
 puts "Bienvenue #{prenom}, que recherchez vous aujourd'hui?"
-print data_hash[0]
+#print data_hash[0,'recipe_name']
+#recette = data_hash[0]
+puts data_hash[0]["recipe_name"]
