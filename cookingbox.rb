8a4382c05728985797f60c671d505949f2fdfe21
JojoDz <72552227+JojoDz@users.noreply.github.com>
2020-11-16 18:51:14 +0100

    comments added day1

--- cookingbox.rb
+++ cookingbox.rb
@@ -1,11 +1,23 @@
+#relier notrefichier.rb au fichier.json
 require 'json'
 file = File.read('recipes.json')
 data_hash = JSON.parse(file)
 
-puts "Bonjour, comment allez vous?"
+#dialogue de début
+puts "Bonjour, comment allez-vous?"
 puts "Quel est votre prénom?"
 prenom = gets.chomp
+#variable pour récupérer le prénom que l'utilisatrice aura saisi
 puts "Bienvenue #{prenom}, que recherchez vous aujourd'hui?"
-#print data_hash[0,'recipe_name']
-#recette = data_hash[0]
+# #{prenom} : sera remplacé par le prénom de l'utilisatrice
+
+#imprimer toute la première recette dans le fichier json sur une même ligne: 
+#print data_hash[0]
+
+# #création d'une variable  pour première recette
+# recette = data_hash[0]
+# #print à l'écran le nom de la première recette
+# puts recette["recipe_name"]
+
+#version + rapide et directe : 
 puts data_hash[0]["recipe_name"]
