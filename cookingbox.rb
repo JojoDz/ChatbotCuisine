a6535224e07096fdfc52f3b9f862b3872be9bf85
JojoDz <72552227+JojoDz@users.noreply.github.com>
2020-11-17 17:17:32 +0100

    parcours liste, affichage champs recipe_id et recipe_name + steps

--- cookingbox.rb
+++ cookingbox.rb
@@ -4,12 +4,40 @@ file = File.read('recipes.json')
 data_hash = JSON.parse(file)
 
 #dialogue de début
-puts "Bonjour, comment allez-vous?"
-puts "Quel est votre prénom?"
-prenom = gets.chomp
-#variable pour récupérer le prénom que l'utilisatrice aura saisi
-puts "Bienvenue #{prenom}, que recherchez vous aujourd'hui?"
-# #{prenom} : sera remplacé par le prénom de l'utilisatrice
+        puts "Hello, comment ça va aujourd'hui ?"
+        puts "C'est quoi ton petit nom ?"
+        prenom = gets.chomp
+        #variable pour récupérer le prénom que l'utilisatrice aura saisi
+        puts "Bienvenu(e) #{prenom}, une petite faim ? ça tombe bien, voici quelques recettes : "
+        #{prenom} : sera remplacé par le prénom de l'utilisatrice
+    # affichage des 3 recettes 
+    data_hash.each do |recette|
+        puts "#{recette["recipe_id"]} - #{recette["recipe_name"]}"
+    end
+
+    puts "Inscris le numéro de la recette qui t'intéresse :"
+    user_choice = gets.chomp
+
+    recettechoice = user_choice
+        
+    recetteinput = data_hash.each do |recette|
+        puts "#{recette["recipe_id"]} - #{recette["recipe_name"]} - #{recette["steps"]}"
+    end
+
+   if user_choice == recettechoice
+        puts recetteinput
+    end
+    # if user_choice == 2
+    #     puts data_hash[1]
+    # end
+    # if user_choice == 3
+    #     puts data_hash[2]
+    # end
+
+
+
+# afficher les 3 recettes
+# l'utilisatrice choisit une de ses recettes
 
 #imprimer toute la première recette dans le fichier json sur une même ligne: 
 #print data_hash[0]
@@ -20,4 +48,12 @@ puts "Bienvenue #{prenom}, que recherchez vous aujourd'hui?"
 # puts recette["recipe_name"]
 
 #version + rapide et directe : 
-puts data_hash[0]["recipe_name"]
+# puts data_hash[0]["recipe_name"]
+
+
+#message erreur si pas choisi de recette 
+# user_choice = gets.chomp
+# while not user_choice == recette1 || recette2 || recette3 do
+#     puts "N'oublie pas de choisir une recette ;-)"
+#     user_choice = gets.chomp
+# puts "Tu as choisi" + user_choice
